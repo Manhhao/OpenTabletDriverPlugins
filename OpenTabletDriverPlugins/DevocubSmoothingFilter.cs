cbc600e5e7b542139c882cfd6878cf1d2f2aae8a
Manh-Hao Do <62149544+Manhhao@users.noreply.github.com>
2020-08-16 21:06:49 +0200

    base

---
+++ OpenTabletDriverPlugins/DevocubSmoothingFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TabletDriverPlugin;
+using TabletDriverPlugin.Attributes;
+using TabletDriverPlugin.Tablet;
+
+namespace OpenTabletDriverPlugins
+{
+    [PluginName("Devocub Smoothing Filter")]
+    class DevocubSmoothingFilter : Notifier, IFilter
+    {
+        public virtual Point Filter(Point point)
+        {
+            return point;
+        }
+        public FilterStage FilterStage => FilterStage.PostTranspose;
+    }
+}
